64203cf313e146058724d9fd182e8bb9f69fefcb
stevenjmesser <ste@renegatus.com>
2024-06-07 05:58:42 +0100

    Fix tags and categories

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 gem 'jekyll'
-gem 'jekyll-paginate'
+gem 'jekyll-paginate-v2'
 gem 'jekyll-seo-tag'
 gem 'jekyll-sitemap'
--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -5,22 +5,24 @@ module Jekyll
     def generate(site)
       tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.to_set
       tags.each do |tag|
-        site.pages << TagPage.new(site, site.source, tag)
+        slug = Jekyll::Utils.slugify(tag)
+        site.pages << TagPage.new(site, site.source, tag, slug)
       end
     end
   end
 
   class TagPage < Page
-    def initialize(site, base, tag)
+    def initialize(site, base, tag, slug)
       @site = site
       @base = base
-      @dir  = File.join('tag', tag)
+      @dir  = File.join('tag', slug)
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
       self.data['title'] = "Tag: #{tag}"
+      self.data['slug'] = slug  # Storing slug for possible use in templates
     end
   end
 end
